bfac2c8cad495e0dff168354d132aa92bd11218e
stanley <stanleyagbanobi@gmail.com>
2025-09-15 00:16:09 +0100

    Modified App

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ export default function App() {
         <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       </div>
 
-      <div className="container mx-auto px-20 md:pl-20">
+      <div className="container mx-auto px-8 md:pl-20">
         <Navbar />
         <Hero />
         <About />
